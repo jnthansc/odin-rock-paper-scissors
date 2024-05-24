e543576c9733c7237eb1959969edd345444235bf
Jonathan Schmidt <jona.schmidt99@web.de>
2024-05-24 16:53:17 +0200

    Write the logic to get the computer choice
    Add a function for generating the computer choice by selecting from a
    choices array via a randomly generated index and return the selected
    entry.

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log('hello');
+
+function getComputerChoice() {
+  const choiceOptions = ['rock', 'paper', 'scissors'];
+  const randomIndex = Math.floor(Math.random() * choiceOptions.length);
+
+  return choiceOptions[randomIndex];
+}
+
+console.log(getComputerChoice());
