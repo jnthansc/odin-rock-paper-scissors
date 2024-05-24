4c4aa7f7d01a2e8dce3fedb9fe54a0590826226f
Jonathan Schmidt <jona.schmidt99@web.de>
2024-05-24 17:07:12 +0200

    Write the logic to play the entire game
    Add a function to play the game for 5 times using a loop. Alert the
    winner by comparing the player scores.

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 console.log('hello');
 
 const choiceOptions = ['rock', 'paper', 'scissors'];
-let humanScore = 0;
-let computerScore = 0;
 
 function getComputerChoice() {
   const randomIndex = Math.floor(Math.random() * choiceOptions.length);
@@ -14,29 +12,44 @@ function getHumanChoice() {
   return prompt('Rock, Paper, Scissors?', '');
 }
 
-function playRound(humanChoice, computerChoice) {
-  humanChoice = humanChoice.toLowerCase();
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
 
-  let winnerAnnouncement = '';
+  for (let i = 0; i < 5; i++) {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
 
-  if (humanChoice === computerChoice) {
-    winnerAnnouncement = "It's a tie! Try again.";
-  } else if (
-    (humanChoice === 'rock' && computerChoice == 'scissors') ||
-    (humanChoice === 'paper' && computerChoice == 'rock') ||
-    (humanChoice === 'scissors' && computerChoice == 'paper')
-  ) {
-    humanScore += 1;
-    winnerAnnouncement = `You win! ${humanChoice} beats ${computerChoice}`;
-  } else {
-    computerScore += 1;
-    winnerAnnouncement = `You lose! ${computerChoice} beats ${humanChoice}`;
+    playRound(humanSelection, computerSelection);
   }
 
-  console.log(winnerAnnouncement);
+  alert(
+    humanScore > computerScore
+      ? 'You won against the computer'
+      : 'You lost against the computer'
+  );
+
+  function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+
+    let winnerAnnouncement = '';
+
+    if (humanChoice === computerChoice) {
+      winnerAnnouncement = "It's a tie! Try again.";
+    } else if (
+      (humanChoice === 'rock' && computerChoice == 'scissors') ||
+      (humanChoice === 'paper' && computerChoice == 'rock') ||
+      (humanChoice === 'scissors' && computerChoice == 'paper')
+    ) {
+      humanScore += 1;
+      winnerAnnouncement = `You win! ${humanChoice} beats ${computerChoice}`;
+    } else {
+      computerScore += 1;
+      winnerAnnouncement = `You lose! ${computerChoice} beats ${humanChoice}`;
+    }
+
+    console.log(winnerAnnouncement);
+  }
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
+playGame();
